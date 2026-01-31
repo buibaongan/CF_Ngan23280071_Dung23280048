a5a145bb5b80c29af6961c55720cca55e5693c02
Ngan <buibaongan@gmail.com>
2026-01-31 17:30:53 +0700

    Update EDA plots and data processor and main

--- src/data/data_processor.py
+++ src/data/data_processor.py
@@ -3,70 +3,77 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats.mstats import winsorize
 
-class DataProcessor:
+
+class DataLoader:
     def __init__(self, file):
         self.file = file
-        self.df = pd.read_csv(self.file, header=[0, 1], index_col=0, parse_dates=True)
-        # Lấy danh sách tickers từ level 1 của columns
-        self.tickers = set(self.df.columns.get_level_values(1))
 
-    def add_log_returns(self):        
-        log_returns = np.log(self.df['Adj Close'] / self.df['Adj Close'].shift(1))
-        log_returns.columns = pd.MultiIndex.from_product([['Log Return'], log_returns.columns])
-        self.df = pd.concat([self.df, log_returns], axis=1)
+    def load(self):
+        return pd.read_csv(self.file, header=[0,1], index_col=0, parse_dates=True)
 
-    def check_na(self, ticker):
-        for col in self.df.columns.levels[0]:
-            na_count = self.df[(col, ticker)].isna().sum()
-            print(f"{col} : {na_count}")
-    
-    def drop_na(self):
-        self.df = self.df.dropna()
-        
-    def detect_outliers(self, ticker, col):
-        series = self.df[(col, ticker)]
-        
+    def save(self, df, output_file):
+        df.to_csv(output_file)
+
+
+class FeatureEngineer:
+    def add_log_returns(self, df):
+        log_returns = np.log(df['Adj Close'] / df['Adj Close'].shift(1))
+        log_returns.columns = pd.MultiIndex.from_product(
+            [['Log Return'], log_returns.columns]
+        )
+        return pd.concat([df, log_returns], axis=1)
+
+
+class DataCleaner:
+    def drop_na(self, df):
+        return df.dropna()
+
+    def winsorize_column(self, df, col, ticker, limits=[0.01,0.01]):
+        series = df[(col, ticker)]
+        mask = ~series.isna()
+        if (len(series[mask]) > 0):
+            df.loc[mask, (col, ticker)] = winsorize(series[mask], limits=limits)
+        return df
+
+
+class OutlierDetector:
+    def detect_iqr(self, series):
         Q1 = series.quantile(0.25)
         Q3 = series.quantile(0.75)
         IQR = Q3 - Q1
-        
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-        
-        outliers = series[(series < lower_bound) | (series > upper_bound)]
-        return outliers
-    
-    def draw_boxplot(self, ticker, col):
-        series = self.df[(col, ticker)]
-        plt.figure()
-        plt.boxplot(series.dropna())
-        plt.title(f"Boxplot of {col} for {ticker}")
-        plt.show()
-    
-    def draw_histogram(self, ticker, col):
-        series = self.df[(col, ticker)]
-        plt.figure()
-        plt.hist(series, bins=50)
-        plt.title(f"Return of {ticker}")
-        plt.show()
+        lb = Q1 - 1.5*IQR
+        ub = Q3 + 1.5*IQR
+        return series[(series < lb) | (series > ub)]
 
-    def winsorize_data(self):
-        for ticker in self.tickers:
-            # Xử lý Log Return
-            if ('Log Return', ticker) in self.df.columns:
-                log_returns = self.df[('Log Return', ticker)]
-                mask_valid = ~log_returns.isna()        # Lọc bỏ NaN
-                data_valid = log_returns[mask_valid]
-                if len(data_valid) > 0:
-                    self.df.loc[mask_valid, ('Log Return', ticker)] = winsorize(data_valid, limits=[0.01, 0.01])
-            
-            # Xử lý Volume
-            if ('Volume', ticker) in self.df.columns:
-                volume = self.df[('Volume', ticker)]
-                mask_valid = ~volume.isna()        # Lọc bỏ NaN
-                data_valid = volume[mask_valid]
-                if len(data_valid) > 0:
-                    self.df.loc[mask_valid, ('Volume', ticker)] = winsorize(data_valid, limits=[0.01, 0.01])
-    
-    def save_cleaned_data(self, output_file):
-        self.df.to_csv(output_file)
+
+class DataProcessor:
+    def __init__(self, file):
+        self.loader = DataLoader(file)
+        self.feature_engineer = FeatureEngineer()
+        self.cleaner = DataCleaner()
+        self.outlier_detector = OutlierDetector()
+
+    def process(self):
+        # Load data
+        df = self.loader.load()
+
+        # Feature engineering
+        df = self.feature_engineer.add_log_returns(df)
+
+        # Winsorize 
+        tickers = set(df.columns.get_level_values(1))
+        for ticker in tickers:
+            if ('Log Return', ticker) in df.columns:
+                df = self.cleaner.winsorize_column(
+                    df, 'Log Return', ticker
+                )
+
+            if ('Volume', ticker) in df.columns:
+                df = self.cleaner.winsorize_column(
+                    df, 'Volume', ticker
+                )
+
+        return df
+
+    def save(self, df, output_file):
+        self.loader.save(df, output_file)
